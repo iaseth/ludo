cc68680af22b158bc8a57a8ab80d213ce0073b22
Ankur Seth <iankurseth@gmail.com>
2022-02-12 15:13:30 +0530

    c0 with react-starter and cleanup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+
+
+function App () {
+	return (
+		<div className="App">
+			<main className="flex min-h-screen bg-blue-500">
+				<div className="text-white text-center m-auto">
+					<a href="https://github.com/iaseth/ludo" target="_blank" rel="noreferrer" className="px-4 py-3 text-sm bg-blue-600 duration-300 hover:bg-blue-700">GitHub</a>
+					<h2 className="text-4xl py-6">Ludo</h2>
+				</div>
+			</main>
+		</div>
+	);
+}
+
+export default App;
